8bc53d1b9332b68b60d002b98d0bfb019d076342
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2024-06-17 13:18:24 +0200

    revert changes in entrypoint

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -18,7 +18,7 @@ if [ $# -eq 0 ]; then
     envsubst < /conf/nginx.conf.tpl > /conf/nginx.conf && nginx -tc /conf/nginx.conf
     envsubst < /conf/redis.conf.tpl > /conf/redis.conf
 
-    python -m django upgrade --no-input
+    django-admin upgrade --no-input
 
     nginx -c /conf/nginx.conf
     redis-server /conf/redis.conf
@@ -29,9 +29,9 @@ else
         "dev")
         until pg_isready -h db -p 5432;
           do echo "waiting for database"; sleep 2; done;
-        python -m django collectstatic --no-input
-        python -m django migrate
-        python -m django runserver 0.0.0.0:8000
+        django-admin collectstatic --no-input
+        django-admin migrate
+        django-admin runserver 0.0.0.0:8000
         ;;
     *)
     exec "$@"
