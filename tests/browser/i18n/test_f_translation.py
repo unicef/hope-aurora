748400f89666a41bf411b87f21f739e7c5b12746
Stefano Apostolico <s.apostolico@gmail.com>
2025-04-20 00:13:48 +0200

    updates tests

--- tests/browser/i18n/test_f_translation.py
+++ tests/browser/i18n/test_f_translation.py
@@ -75,5 +75,5 @@ def test_import_translate(mock_state, browser: AuroraTestBrowser, registration):
     browser.click("input#import[type=submit]")
 
     browser.click("input#save")
-    time.sleep(0.3)
+    time.sleep(0.5)
     assert browser.get_text("ul.messagelist") == "Messages processed: Processed: 5, Selected: 5, Created: 5, Updated: 0"
