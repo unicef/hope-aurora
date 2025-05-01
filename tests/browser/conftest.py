3538b687a86776574477e229bd8e5c1f10f122c2
sax <s.apostolico@gmail.com>
2025-05-01 12:00:25 +0200

    fixes selenium tetss

--- tests/browser/conftest.py
+++ tests/browser/conftest.py
@@ -1,30 +1,30 @@
-from typing import Generator
+from typing import TYPE_CHECKING, Generator
 
 import pytest
 from seleniumbase import config as sb_config
 from seleniumbase.core import session_helper
-from testutils.factories import ValidatorFactory
-from testutils.selenium import AuroraSeleniumTC
 
-from aurora.core.models import Validator
-from aurora.state import State
+if TYPE_CHECKING:
+    from testutils.selenium import AuroraSeleniumTC
+
+    from aurora.state import State
 
 
 @pytest.fixture
-def mock_state(rf) -> Generator[State, None, None]:
+def mock_state(rf) -> Generator["State", None, None]:
     from aurora.state import state
 
     state.request = rf.get("/")
-    # -state.user = AnonymousUser()
     yield state
     state.request = None
 
 
 @pytest.fixture
-def browser(live_server, request) -> Generator[AuroraSeleniumTC, None, None]:
+def browser(live_server, request) -> "Generator[AuroraSeleniumTC, None, None]":
     """SeleniumBase as a pytest fixture.
     Usage example: "def test_one(sb):"
     You may need to use this for tests that use other pytest fixtures."""
+    from testutils.selenium import AuroraSeleniumTC
 
     if request.cls:
         if sb_config.reuse_class_session:
@@ -60,6 +60,8 @@ def browser(live_server, request) -> Generator[AuroraSeleniumTC, None, None]:
 
 @pytest.fixture
 def birth_after_1900(db):
+    from testutils.factories import Validator, ValidatorFactory
+
     code = """
     var limit1 = Date.parse("1900-01-01");
     var today = new Date();
