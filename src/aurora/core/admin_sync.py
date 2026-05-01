aeb015ad6b0d14b9dc0dccfc9beea3925847d050
roma-valor <roma.gudavadze@valor-software.com>
2026-05-01 11:23:46 +0400

    fix: add admin_filters_media in init

--- src/aurora/core/admin_sync.py
+++ src/aurora/core/admin_sync.py
@@ -1,5 +1,7 @@
 from admin_sync.conf import config
 from admin_sync.mixins import SyncModelAdmin
+from django import forms
+from django.contrib.admin import AdminSite
 from django.db.models import Model
 from django.http import HttpRequest
 
@@ -15,6 +17,10 @@ def is_remote(request: HttpRequest) -> bool:
 class SyncMixin(SyncModelAdmin):
     actions: tuple[str, ...] = ("publish_action",)
     UPDATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+    def __init__(self, model: type[Model], admin_site: AdminSite) -> None:
+        self.admin_filters_media = forms.Media()
+        super().__init__(model, admin_site)
 
     def can_publish(self, request: HttpRequest, pk: str | None = None, obj: Model | None = None) -> bool:
         return super().can_publish(request, pk, obj)
